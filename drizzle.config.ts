9e1e5fd0359ff224a53fa41e9afadee230d26f02
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-10 05:55:25 -0300

    feat: add docker compose file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'drizzle-kit';
-import { env } from '@/env';
+import { env } from '@/env.js';
 
 export default defineConfig({
 	dialect: 'postgresql',
--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { env } from 'process';
+import { env } from '@/env.js'
 import { schema } from './schema/index.js';
 
-export const db = drizzle(env.DATABASE_URL, { schema, casing: 'snake_case' });
+export const db = drizzle(env.DATABASE_URL, { schema: schema, casing: 'snake_case' });
