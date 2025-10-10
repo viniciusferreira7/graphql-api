1668370e79e98d83312a7ccb7ddeb2535e39f1e2
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-10 05:45:32 -0300

    feat: add zod to validade envs and typescript config

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from '@/env';
+
+export default defineConfig({
+	dialect: 'postgresql',
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	schema: 'src/db/schema/*',
+	out: 'src/db/migrations',
+	casing: 'snake_case',
+});
+
+//TODO: add zod and env validation
--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { env } from '@/env';
-import { schema } from './schema';
+import { env } from 'process';
+import { schema } from './schema/index.js';
 
 export const db = drizzle(env.DATABASE_URL, { schema, casing: 'snake_case' });
